c578583077b7ede220fedabf5e7b4d9dc24a19a2
Ben Mills <fuzzyllama@gmail.com>
2022-03-07 16:33:11 -0800

    Adds application properties and user model

---
+++ src/main/java/com/akkanben/codefellowship/model/ApplicationUser.java
@@ -0,0 +1,84 @@
+package com.akkanben.codefellowship.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class ApplicationUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    Date dateOfBirth;
+    @Column(columnDefinition = "text")
+    String bio;
+
+    public ApplicationUser() {
+
+    }
+
+    public ApplicationUser(String username, String password, String firstName, String lastName, Date dateOfBirth, String bio) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.bio = bio;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
